799e4e31fa4e45227508864f3efd27c33634d609
r41L <stephanbrummel@online.de>
2017-12-18 11:15:50 +0000

    Fehlerbeseitigung und Konstruktor clsInvoicePosition

--- UrbanInvoicingLibraryBUMS/clsInvoicePosition.cs
+++ UrbanInvoicingLibraryBUMS/clsInvoicePosition.cs
@@ -2,21 +2,34 @@
 
 namespace UrbanInvoicingLibraryBUMS
 {
-    public class clsInvoicePosition
+    public class clsInvoicePosition :clsDatabaseObject
     {
         #region Attribute
         int id, invoiceId, typeId, artikelId;
         string bemerkung;
         double brutto, netto, mwst, rabat;
 
+        public clsType type { get; set; }   
+        public clsArticel articel { get; set; }
         public clsInvoice invoice {get; set;}
-        clsType
-        clsArtikel artikel;
         #endregion
 
         #region Kontruktor
         public clsInvoicePosition()
 	    {	}
+
+        public clsInvoicePosition(int pId, int pInvoiceId, int pTypeId, int pArtikelId, string pBemerkung,double pBrutto, double pNetto, double pMwst, double pRabat)
+        {
+            this.id=pId;
+            this.invoiceId = pInvoiceId;
+            this.typeId = pType;
+            this.artikelId = pArtikelId;
+            this.bemerkung = pBemerkung;
+            this.brutto = pBrutto;
+            this.netto = pNetto; 
+            this.mwst = pMwst; 
+            this.rabat = pRabat;
+        }
         #endregion
     }
 }
